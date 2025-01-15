084cdc63ef682b7fd200551a150d5e126aec17b2
Sravanthi Yachareni <sravanthiyachareni@gmail.com>
2025-01-15 09:43:20 +0530

    added

--- src/component/Student.js
+++ src/component/Student.js
@@ -1,41 +1,75 @@
-import React from 'react'
+import React, { useState } from 'react'
  import './style.css'
  function Student() {
+   const [values,setValues] = useState( {
+      firstname:'',
+      lastname: '',
+      email: '',
+      gender: '',
+      select: '',
+      resume: '',
+      about: ''
+
+
+   })
+   const handleChanges = (e) =>  {
+    const {name,value} = e.target
+       setValues(
+         {
+            ...values,
+            [name]:value
+         }
+       )
+   }
+   const handleSubmit = (e) => {
+      e.preventDefault()
+      console.log(values)
+
+   }
+   const resetHandle = () => {
+      setValues(
+         {
+            firstname: '',
+            lastname: '',
+            email: ''
+         }
+      )
+   }
   return (
-   <form className='student'>
+   <form className='student' onSubmit={handleSubmit}>
     <h1>Student Registration Form</h1>
     <div>
     <label>First Name:</label>
-    <input  type='text'  placeholder='Enter First Name'name='firstname'/>
+    <input  type='text'  placeholder='Enter First Name'name='firstname' onChange={(e) =>handleChanges(e)}/>
     </div>
     <div>
     <label >Last Name:</label>
-    <input  type='text' placeholder='Enter Last Name' name='lastname'/>
+    <input  type='text' placeholder='Enter Last Name' name='lastname' onChange={(e) =>handleChanges(e)}/>
     </div>
     <div>
     <label>Email:</label>
-    <input  type='text' placeholder='Enter Email' name='email'/>
+    <input  type='text' placeholder='Enter Email' name='email' onChange={(e) =>handleChanges(e)} required/>
     </div>
     <div>
     <label>Gender</label>
-    <input type='radio'name='gender'/>Male
-     <input type='radio' name='gender'/>Female
-     <input type='radio' name='gender'/>Other
+    <input type='radio'name='gender' onChange={(e) =>handleChanges(e)}/>Male
+     <input type='radio' name='gender' onChange={(e) =>handleChanges(e)}/>Female
+     <input type='radio' name='gender' onChange={(e) =>handleChanges(e)}/>Other
      </div>
      <div>
         <label>Select Course</label>
-     <select> 
+     <select name='select' onChange={(e) =>handleChanges(e)}> 
         <option>JAVA</option>
         <option>C++</option>
      </select>
      </div>
      <div>
      <label>Upload resume</label>
-     <input type='file'/>
+     <input type='file' name="resume" onChange={(e) =>handleChanges(e)}/>
      </div>
     <label>About</label>
-    <textarea id='about' cols="30" rows="10"></textarea>
-    <button>Reset</button>
+    <textarea  name = "about" id='about' cols="30" rows="10" onChange={(e) =>handleChanges(e)}></textarea>
+    <button onClick={resetHandle}>Reset</button>
     <button>Submit</button>
    </form>
   )
